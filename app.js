30499d3f020ffcb73d7ab525efc324736c6937ed
Facundo Meniy <facundomeniy@gmail.com>
2022-12-01 17:13:59 -0300

    wip

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const allCells = document.querySelectorAll(".cell");
-const cell1 = document.getElementById("one");
-const cell2 = document.getElementById("two");
-const cell3 = document.getElementById("three");
-const cell4 = document.getElementById("four");
-const cell5 = document.getElementById("five");
-const cell6 = document.getElementById("six");
-const cell7 = document.getElementById("seven");
-const cell8 = document.getElementById("eight");
-const cell9 = document.getElementById("nine");
+const cell0 = document.getElementById("one");
+const cell1 = document.getElementById("two");
+const cell2 = document.getElementById("three");
+const cell3 = document.getElementById("four");
+const cell4 = document.getElementById("five");
+const cell5 = document.getElementById("six");
+const cell6 = document.getElementById("seven");
+const cell7 = document.getElementById("eight");
+const cell8 = document.getElementById("nine");
+const resetButton = document.getElementById("reset-button");
 //
 let player1turn = true;
 let isGameOver = false;
@@ -20,50 +21,92 @@ const playerFactory = (name, sign) => {
 
 const gameboard = (() => {
     const board = ["", "", "", "", "", "", "", "", ""];
+
     const update = () => {
-        cell1.textContent = board[0];
-        cell2.textContent = board[1];
-        cell3.textContent = board[2];
-        cell4.textContent = board[3];
-        cell5.textContent = board[4];
-        cell6.textContent = board[5];
-        cell7.textContent = board[6];
-        cell8.textContent = board[7];
-        cell9.textContent = board[8];
-    };
+        board[0] = cell0.textContent;
+        board[1] = cell1.textContent;
+        board[2] = cell2.textContent;
+        board[3] = cell3.textContent;
+        board[4] = cell4.textContent;
+        board[5] = cell5.textContent;
+        board[6] = cell6.textContent;
+        board[7] = cell7.textContent;
+        board[8] = cell8.textContent;
+    }
+
+    const winConditions = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
+    ];
     
     return {
       board,
       update,
+      winConditions,
     };
   })();
 
 const player1 = playerFactory('Player 1', "X");
 const player2 = playerFactory('Player 2', "O");
 
-function placeMarker(sign){
-    for(let cell of allCells){
-        cell.addEventListener('click', () => {
-            cell.textContent = sign;
-        })
-    }
-};
-
 function changeTurn(){
     player1turn = !player1turn;
 };
 
-for(let cell of allCells){
-    cell.addEventListener('click', () => {
-        if(player1turn === true){
-            placeMarker(player1.getSign());
-            changeTurn();
-        }else{
-            placeMarker(player2.getSign());
-            changeTurn();
+function checkWin(){
+    for(let condition of gameboard.winConditions){
+        const cellA = gameboard.board[condition[0]];
+        const cellB = gameboard.board[condition[1]];
+        const cellC = gameboard.board[condition[2]];
+
+        if(cellA === "" || cellB === "" || cellC === ""){
+            continue;
         }
-    })
-}
+        if(cellA === cellB && cellB && cellC){
+            isGameOver = true;
+            break;
+        }
+    }
+    if(isGameOver === true){
+        for(let cell of allCells){
+            cell.classList.add("disable");
+        }
+    }
+ }
 
+for(let cell of allCells){
+     cell.addEventListener('click', () => {
+        if(player1turn === true){
+            cell.textContent = player1.getSign();
+            cell.classList.add("player-one");
+            changeTurn();
+            gameboard.update();
+            checkWin();
+        }else{
+            cell.textContent = player2.getSign();
+            cell.classList.add("player-two");
+            changeTurn();
+            gameboard.update();
+            checkWin();
+        }
+     })
+ }
 
+ resetButton.addEventListener('click', () => {
+    gameboard.board = ["", "", "", "", "", "", "", "", ""];
+    isGameOver = false;
+    player1turn = true;
 
+    for(let cell of allCells){
+        cell.textContent = "";
+        cell.classList.remove("player-one");
+        cell.classList.remove("player-two");
+        cell.classList.remove("disable");
+    }
+ })
